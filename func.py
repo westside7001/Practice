27a9ac2415f98c2530ed8d5966ea7a69f3026b58
Curtis Hsu <westside7001@gmail.com>
2025-06-09 10:28:32 -0700

    First time to learn function

---
+++ func.py
@@ -0,0 +1,16 @@
+# function 函式/功能
+# function是用來收納程式碼的
+# 它是個功能
+
+def wash():
+    print('加水')
+    print('加洗衣精')
+    print('旋轉')
+
+
+wash()  # 使用function
+
+def say_hi():
+    print('hi!')
+
+say_hi()
