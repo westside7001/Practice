4d1153808523118b50c5037713e3148ef04c0e57
Curtis Hsu <westside7001@gmail.com>
2025-06-08 21:01:05 -0700

    Calculate comment length

---
+++ read.py
@@ -0,0 +1,15 @@
+data = []
+count = 0
+with open('reviews.txt','r') as file:
+    for line in file:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+print('檔案讀取完畢，總共有',len(data),'筆資料')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+
+print('留言平均長度為',sum_len/len(data))
