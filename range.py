b2cb5ba073ab80d97a4af53059ab9e3ffdcb2ce5
Curtis Hsu <westside7001@gmail.com>
2025-06-08 22:18:02 -0700

    first commit for range.py

---
+++ range.py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(10):
+    q = random.randint(1,500)
+    print('這是第',i+1,'次產生的隨機數',q)
--- read.py
+++ read.py
@@ -22,3 +22,9 @@ print('一共有',len(new),'筆留言長度小於100')
 print(new[0])
 print(new[0][0][0])
 
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+
+print('一共有',len(good), '筆留言中內含"good"這個字')
